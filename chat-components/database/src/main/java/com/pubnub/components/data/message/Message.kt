3278f144dd8d60cc61ccc9170ad549075fe80ed6
Marcin Środa <marcin.sroda@pubnub.com>
2022-06-13 11:10:49 +0200

    Review changes

--- chat-components/database/src/main/java/com/pubnub/components/data/message/Message.kt
+++ chat-components/database/src/main/java/com/pubnub/components/data/message/Message.kt
@@ -9,9 +9,4 @@ interface Message {
     val content: Any?           // Extra content for the message. Can contain any feature-specific data, e.g. URLs to external images.
     val createdAt: String       // ISO8601 date string of when the message was created
     val custom: Any?            // Any additional payload possibly needed and provided by end users
-//    val publisher: UserId
-//    val channel: ChannelId
-//    val timetoken: Timetoken
-//    val isSent: Boolean
-//    var exception: String?
 }
