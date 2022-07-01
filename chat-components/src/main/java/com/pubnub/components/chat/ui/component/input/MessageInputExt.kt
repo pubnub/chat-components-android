34d805eef64c16fbd186d01a3c974d356e562be1
Marcin Środa <marcin.sroda@pubnub.com>
2022-07-01 17:52:18 +0200

    Typing Indicator refactor,

--- chat-components/src/main/java/com/pubnub/components/chat/ui/component/input/MessageInputExt.kt
+++ chat-components/src/main/java/com/pubnub/components/chat/ui/component/input/MessageInputExt.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
@@ -12,21 +13,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
 import com.pubnub.components.R
 import com.pubnub.components.chat.network.data.NetworkMessagePayload
+import com.pubnub.components.chat.ui.component.input.renderer.AnimatedTypingIndicatorRenderer
 import com.pubnub.components.chat.ui.component.input.renderer.DefaultTypingIndicatorRenderer
 import com.pubnub.components.chat.ui.component.input.renderer.TypingIndicatorRenderer
+import com.pubnub.components.chat.ui.component.input.renderer.TypingState
 import com.pubnub.components.chat.ui.component.provider.LocalChannel
 import com.pubnub.components.chat.ui.component.provider.LocalPubNub
 import com.pubnub.framework.service.LocalTypingService
 import com.pubnub.framework.util.Timetoken
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.map
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MessageInput(
     initialText: String = "",
     placeholder: String = stringResource(id = R.string.type_message),
-    typingIndicator: Boolean = false,
-    typingIndicatorRenderer: TypingIndicatorRenderer = DefaultTypingIndicatorRenderer,
+    typingIndicatorEnabled: Boolean = false,
+    typingIndicatorContent: @Composable ColumnScope.(TypingState) -> Unit = { state ->
+        AnimatedVisibility(
+            visible = state.data.isNotEmpty(),
+            enter = expandVertically(),
+            exit = shrinkVertically(),
+        ) {
+            AnimatedTypingIndicatorRenderer.TypingIndicator(state.data)
+        } },
     onSuccess: (String, Timetoken) -> Unit = { _, _ -> },
     onError: (Exception) -> Unit = {},
 ) {
@@ -35,11 +46,11 @@ fun MessageInput(
     val channel = LocalChannel.current
 
     val viewModel: MessageInputViewModel =
-        MessageInputViewModel.default(typingService = if (typingIndicator) LocalTypingService.current else null)
+        MessageInputViewModel.default(typingService = if (typingIndicatorEnabled) LocalTypingService.current else null)
 
     // region actions
     val typingAction: (String) -> Unit = { message ->
-        if (typingIndicator)
+        if (typingIndicatorEnabled)
             viewModel.setTyping(channel, message.isNotEmpty())
     }
 
@@ -58,22 +69,18 @@ fun MessageInput(
     // endregion
 
     Column {
-        if (typingIndicator) {
+        if (typingIndicatorEnabled) {
             val typingService = LocalTypingService.current
             DisposableEffect(channel) {
                 typingService.bind(channel)
                 onDispose { typingService.unbind() }
             }
 
-            val typing by typingService.getTyping(channel)
-                .collectAsState(initial = emptyList())
-            AnimatedVisibility(
-                visible = typing.isNotEmpty(),
-                enter = expandVertically(),
-                exit = shrinkVertically(),
-            ) {
-                typingIndicatorRenderer.TypingIndicator(data = typing)
-            }
+            val typing by typingService.getTyping(channel).collectAsState(initial = emptyList())
+            val state = TypingState(typing)
+
+
+            typingIndicatorContent(state)
         }
         MessageInput(
             placeholder = placeholder,
---
+++ chat-components/ui/src/main/java/com/pubnub/components/chat/ui/component/input/renderer/TypingState.kt
@@ -0,0 +1,5 @@
+package com.pubnub.components.chat.ui.component.input.renderer
+
+import com.pubnub.framework.data.Typing
+
+data class TypingState(val data: List<Typing>)
